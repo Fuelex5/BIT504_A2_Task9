8167088e66e270e5c018558ebadf14da06622791
Fuelex5 <cwgroom@hotmail.com>
2025-05-03 18:14:51 +1200

    Adding the first version of the Java code

---
+++ code.java
@@ -0,0 +1,10 @@
+package Github;
+
+public class code {
+	public static void main(String[] args) {
+		
+		System.out.println("This is a few lines of code");
+		System.out.println("The purpose is to check whether the Github repository is working");
+
+}
+}
